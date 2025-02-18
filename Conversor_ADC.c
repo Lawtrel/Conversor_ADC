12130acb4ce34f9068d399136a15107151098089
Lawtrel <kazemarus@outlook.com>
2025-02-18 15:19:47 -0300

    feat: finalizado

--- Conversor_ADC.c
+++ Conversor_ADC.c
@@ -18,7 +18,8 @@
 #define BTN_A 5
 #define I2C_SDA 14
 #define I2C_SCL 15
-
+#define DEADZONE 50
+#define MOVING_AVG_SIZE 5
 ssd1306_t display;
 uint8_t ssd[ssd1306_buffer_length];
 struct render_area frame_area = {
@@ -27,9 +28,14 @@ bool led_green_state = false;
 bool pwm_enabled = true;
 int border_style= 0;
 
+int adc_x_buffer[MOVING_AVG_SIZE];
+int adc_y_buffer[MOVING_AVG_SIZE];
+int buffer_index_x = 0, buffer_index_y = 0;
+
 void init_display();
 void update_display(int x, int y);
 void draw_border();
+int moving_average(int *buffer, int new_value, int *index);
 
 void button_callback(uint gpio, uint32_t events) {
     static uint32_t last_time = 0;
@@ -41,7 +47,6 @@ void button_callback(uint gpio, uint32_t events) {
     {
         led_green_state = !led_green_state;
         gpio_put(GREEN_PIN, led_green_state);
-
         //altera a borda do display
         border_style = (border_style + 1) % 3;
         draw_border();
@@ -89,6 +94,14 @@ void setup() {
     gpio_set_dir(BTN_A, GPIO_IN);
     gpio_pull_up(BTN_A);
     gpio_set_irq_enabled_with_callback(BTN_A, GPIO_IRQ_EDGE_FALL, true, &button_callback);
+
+    for (int i = 0; i < MOVING_AVG_SIZE; i++) {
+        adc_select_input(0);
+        adc_x_buffer[i] = adc_read();
+        adc_select_input(1);
+        adc_y_buffer[i] = adc_read();
+    }
+
     //inicializa o display
     init_display();
     draw_border();
@@ -98,12 +111,16 @@ int main() {
     int x, y;
     while (1) {
         adc_select_input(0);
-        int adc_x = adc_read();
+        int raw_adc_x = adc_read();
         adc_select_input(1);
-        int adc_y = adc_read();
+        int raw_adc_y = adc_read();
 
-        x = (adc_y * 120) / 4095;
-        y = (adc_x * 56) / 4095;
+        int adc_x = moving_average(adc_x_buffer, raw_adc_x, &buffer_index_x);
+        int adc_y = moving_average(adc_y_buffer, raw_adc_y, &buffer_index_y);
+
+        
+        x = ((adc_y - 2048) * 58) / 2048 + 63;
+        y = ((2048 - adc_x) * 30) / 2048 + 32;
 
         if (pwm_enabled) {
             if (adc_x != 2048) pwm_set_gpio_level(RED_PIN, adc_x);
@@ -116,6 +133,16 @@ int main() {
         sleep_ms(100);
     }
     
+}
+int moving_average(int *buffer, int new_value, int *index) {
+    buffer[*index] = new_value;
+    *index = (*index + 1) % MOVING_AVG_SIZE;
+    
+    int sum = 0;
+    for (int i = 0; i < MOVING_AVG_SIZE; i++) {
+        sum += buffer[i];
+    }
+    return sum / MOVING_AVG_SIZE;
 }
 
 void init_display() {
@@ -132,6 +159,7 @@ void init_display() {
 }
 
 void update_display(int x, int y) {
+    char buffer[20];
     memset(ssd, 0, ssd1306_buffer_length);
     draw_border();
     for (int i = 0; i < 8; i++) {
@@ -139,6 +167,8 @@ void update_display(int x, int y) {
             ssd1306_set_pixel(ssd, x + i, y + j, true);
         }
     }
+    sprintf(buffer, "X: %d Y: %d", x,y);
+    ssd1306_draw_string(ssd, 5, 50, buffer);
     render_on_display(ssd, &frame_area);
 }
 
@@ -154,5 +184,5 @@ void draw_border() {
             ssd1306_set_pixel(ssd, 0, i, true);
             ssd1306_set_pixel(ssd, ssd1306_width - 1, i , true);
         }
-    }
+    }    
 }
